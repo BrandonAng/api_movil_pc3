58accf6faafd2dcb28864c729122e672b7e4f012
brangels78 <brangels78@gmail.com>
2020-12-04 11:46:20 -0500

    Mi primer commit

---
+++ src/main/java/com/redsocial/repository/LibroRepository.java
@@ -0,0 +1,15 @@
+package com.redsocial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.redsocial.entity.Libro;
+
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+	
+	@Query("select x from Libro x where x.codigo = :var_filtro")
+	public List<Libro> listaPorCodigo(@Param("var_filtro") String filtro);
+}
---
+++ src/main/java/com/redsocial/service/LibroService.java
@@ -0,0 +1,11 @@
+package com.redsocial.service;
+
+import java.util.List;
+
+import com.redsocial.entity.Libro;
+
+public interface LibroService {
+
+	public List<Libro> listaLibro();
+	public List<Libro> listaLibroPorCodigo(String codigo);
+}
